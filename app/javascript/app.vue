653badb86511f9ab74d7f14af1ecc9ce6cb42180
児山直人 <naoto.koyama@uhuru.jp>
2020-03-24 22:21:23 +0900

    add sort

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,12 +3,12 @@
     <table>
       <thead>
         <tr>
-          <th>コード</th>
-          <th>銘柄</th>
-          <th>会計年度</th>
-          <th>配当金</th>
-          <th>配当性向</th>
-          <th>連続増配年数</th>
+          <th @click="sort('code')">コード</th>
+          <th @click="sort('name')">銘柄</th>
+          <th @click="sort('latest_fiscal_year')">会計年度</th>
+          <th @click="sort('latest_dividend')">配当金</th>
+          <th @click="sort('payout_ratio')">配当性向</th>
+          <th @click="sort('continuous_dividend_increase_years')">連続増配年数</th>
         </tr>
       </thead>
       <tbody>
@@ -27,16 +27,33 @@
 
 <script>
 import axios from 'axios'
+import _ from 'lodash'
 export default {
   data: function () {
     return {
-      companies: []
+      companies: [],
+      sort_column: '',
+      sort_order: 'asc'
     }
   },
   mounted () {
     axios
       .get('/api/dividends.json')
-      .then(response => (this.companies = response.data))
+      .then(response => {
+        this.companies = response.data
+        this.sort('code')
+      })
+  },
+  methods : {
+    sort (select_column) {
+      this.sort_order = this.getSortOrder(select_column === this.sort_column)
+      this.sort_column = select_column
+      this.companies = _.orderBy(this.companies, this.sort_column, this.sort_order)
+    },
+    getSortOrder(isChangeOrder) {
+      if (!isChangeOrder) return this.sort_order
+      return this.sort_order === 'asc' ? 'desc' : 'asc'
+    }
   }
 }
 </script>
