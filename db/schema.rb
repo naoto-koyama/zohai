b45d9bdb9f01cc387c19cf04ae0544021c75c8f0
児山直人 <naoto.koyama@uhuru.jp>
2020-03-24 10:13:53 +0900

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_003759) do
+ActiveRecord::Schema.define(version: 2020_03_24_010341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_03_24_003759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "continuous_dividend_increase_years", default: 0, null: false
+    t.decimal "payout_ratio"
     t.index ["company_fiscal_year_id"], name: "index_dividends_on_company_fiscal_year_id"
   end
 
