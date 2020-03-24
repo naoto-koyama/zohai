54e2f31187d77393bcbc5aed48a79f74f7a30f50
児山直人 <naoto.koyama@uhuru.jp>
2020-03-24 10:14:31 +0900

    add payout_ratio to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,6 @@ ActiveRecord::Base.transaction do
     (1..10).reverse_each do |n|
       fiscal_year = FiscalYear.where(fiscal_year: date_of_fiscal_year.ago(n.years)).first
       surplus_dividend = Faker::Number.between(from: 1_000_000, to: 1_000_000_000)
-      buyback = surplus_dividend * Faker::Number.between(from: 0, to: 1) * Faker::Number.between(from: 1, to: 100) / 100
       indicated_dividend = Faker::Number.decimal(l_digits: 2, r_digits: 2)
 
       company_fiscal_year = create(:company_fiscal_year,
@@ -41,7 +40,8 @@ ActiveRecord::Base.transaction do
         company_fiscal_year: company_fiscal_year,
         indicated_dividend: indicated_dividend,
         surplus_dividend: surplus_dividend,
-        buyback: buyback,
+        buyback: (surplus_dividend * Faker::Number.between(from: 0, to: 1) * Faker::Number.between(from: 1, to: 100) / 100),
+        payout_ratio: Faker::Number.decimal(l_digits: 2, r_digits: 2),
         continuous_dividend_increase_years: _continuous_dividend_increase_years
       )
     end
