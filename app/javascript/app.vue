7e429e66e8220d532490fadaa83652ea7da0ca5d
児山直人 <naoto.koyama@uhuru.jp>
2020-03-26 23:05:00 +0900

    デザインイメージの実装 　・ページネーションのlimit数を15に変更 　・高さも変更

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,52 +1,65 @@
 <template>
-  <div class="p-table-list">
-    <table class="p-divide-list" :class="{ 'is_show_detail': is_show_detail }">
-      <thead>
-        <tr>
-          <th :class="orderClass('code')" @click="sort('code') ">コード</th>
-          <th :class="orderClass('name')" @click="sort('name')">銘柄</th>
-          <th :class="orderClass('fiscal_year')" @click="sort('fiscal_year')">会計年度</th>
-          <th :class="orderClass('indicated_dividend')" @click="sort('indicated_dividend')">配当金</th>
-          <th :class="orderClass('payout_ratio')" @click="sort('payout_ratio')">配当性向</th>
-          <th :class="orderClass('continuous_dividend_increase_years')" @click="sort('continuous_dividend_increase_years')">連続増配</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="hoverable" v-for="brand_latest_dividend in brand_latest_dividends.slice(0, 100)" :key="brand_latest_dividend.id"　@click="clickBrandName(brand_latest_dividend.id)">
-          <td>{{brand_latest_dividend.code}}</td>
-          <td class="p-brand-name--truncate" >{{brand_latest_dividend.name}}</td>
-          <td>{{brand_latest_dividend.fiscal_year}}</td>
-          <td>{{brand_latest_dividend.indicated_dividend}}</td>
-          <td>{{brand_latest_dividend.payout_ratio}}</td>
-          <td>{{brand_latest_dividend.continuous_dividend_increase_years}}</td>
-        </tr>
-      </tbody>
-    </table>
-
-    <transition name="fade">
-      <div class="p-divide-trend-list" v-if="is_show_detail">
-        <i class="material-icons p-close" @click="clickCloseIcon()">close</i>
+  <main>
+    <div class="p-table-list">
+      <div class="p-divide-list" :class="{ 'is_show_detail': is_show_detail }">
         <table>
           <thead>
             <tr>
-              <th>会計年度</th>
-              <th>配当金</th>
-              <th>配当性向</th>
-              <th>連続増配</th>
+              <th :class="orderClass('code')" @click="sort('code') ">コード</th>
+              <th :class="orderClass('name')" @click="sort('name')">銘柄</th>
+              <th :class="orderClass('fiscal_year')" @click="sort('fiscal_year')">会計年度</th>
+              <th :class="orderClass('indicated_dividend')" @click="sort('indicated_dividend')">配当金</th>
+              <th :class="orderClass('payout_ratio')" @click="sort('payout_ratio')">配当性向</th>
+              <th :class="orderClass('continuous_dividend_increase_years')" @click="sort('continuous_dividend_increase_years')">連続増配</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="dividend_trend in dividend_trends" :v-key="dividend_trend.fiscal_year">
-              <td>{{dividend_trend.fiscal_year}}</td>
-              <td>{{dividend_trend.indicated_dividend}}</td>
-              <td>{{dividend_trend.payout_ratio}}</td>
-              <td>{{dividend_trend.continuous_dividend_increase_years}}</td>
+            <tr class="hoverable" v-for="brand_latest_dividend in brand_latest_dividends.slice(0, 15)" :key="brand_latest_dividend.id"　@click="clickBrandName(brand_latest_dividend.id)">
+              <td>{{brand_latest_dividend.code}}</td>
+              <td class="p-brand-name--truncate" >{{brand_latest_dividend.name}}</td>
+              <td>{{brand_latest_dividend.fiscal_year}}</td>
+              <td>{{brand_latest_dividend.indicated_dividend}}</td>
+              <td>{{brand_latest_dividend.payout_ratio}}</td>
+              <td>{{brand_latest_dividend.continuous_dividend_increase_years}}</td>
             </tr>
           </tbody>
         </table>
+        <ul class="pagination">
+          <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+          <li class="active"><a href="#!">1</a></li>
+          <li class="waves-effect"><a href="#!">2</a></li>
+          <li class="waves-effect"><a href="#!">3</a></li>
+          <li class="waves-effect"><a href="#!">4</a></li>
+          <li class="waves-effect"><a href="#!">5</a></li>
+          <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
       </div>
-    </transition>
-  </div>
+
+      <transition name="fade">
+        <div class="p-divide-trend-list" v-if="is_show_detail">
+          <i class="material-icons p-close" @click="clickCloseIcon()">close</i>
+          <table>
+            <thead>
+              <tr>
+                <th>会計年度</th>
+                <th>配当金</th>
+                <th>配当性向</th>
+                <th>連続増配</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="dividend_trend in dividend_trends" :v-key="dividend_trend.fiscal_year">
+                <td>{{dividend_trend.fiscal_year}}</td>
+                <td>{{dividend_trend.indicated_dividend}}</td>
+                <td>{{dividend_trend.payout_ratio}}</td>
+                <td>{{dividend_trend.continuous_dividend_increase_years}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </transition>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -101,6 +114,10 @@ export default {
 
   table {
     white-space:nowrap;
+
+    tr {
+      line-height: 0.5;
+    }
   }
 
   .p-divide-list {
@@ -117,6 +134,10 @@ export default {
         width: 100%;
       }
     }
+  }
+
+  .pagination {
+    text-align: center;
   }
 
   .p-divide-trend-list {
