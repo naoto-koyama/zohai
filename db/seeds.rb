2e8e8c85e29a12be4027ddbb68a18a3ebd1f0841
児山直人 <naoto.koyama@uhuru.jp>
2020-03-31 21:45:23 +0900

    SeedでBulk Insert処理を実装およびバグの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,34 @@ def continuous_dividend_increase_years(_continuous_dividend_increase_years, indi
 end
 
 ActiveRecord::Base.transaction do
-  (1..12).each do |month|
-    (1..10).each do |n|
-      FiscalYear.create(fiscal_year: Date.new(Time.current.ago(n.years).year, month, 1))
-    end
+  fiscal_years = []
+  (0..132).each do |month|
+    create_time = Time.current.ago(month.months)
+    fiscal_years << FiscalYear.new(fiscal_year: Date.new(create_time.year, create_time.month, 1))
   end
+  FiscalYear.import!(fiscal_years)
 
-  dividend = nil
+  brands = []
   100.times do |n|
-    brand = Brand.create(
+    brands << Brand.new(
       code: Faker::Number.unique.number(digits: 4).to_s,
       name: Faker::Company.unique.name
     )
+  end
+  Brand.import!(brands)
 
+  dividend = nil
+  Brand.all.each do |brand|
     _continuous_dividend_increase_years = 0
     date_of_fiscal_year = Faker::Date.in_date_period.beginning_of_month
     (1..10).reverse_each do |n|
       fiscal_year = FiscalYear.where(fiscal_year: date_of_fiscal_year.ago(n.years)).first
+      brand_fiscal_year = BrandFiscalYear.create!(brand: brand, fiscal_year: fiscal_year)
+
       surplus_dividend = Faker::Number.between(from: 1_000_000, to: 1_000_000_000)
       indicated_dividend = Faker::Number.decimal(l_digits: 2, r_digits: 2)
-
-      brand_fiscal_year = BrandFiscalYear(brand: brand, fiscal_year: fiscal_year)
-
       _continuous_dividend_increase_years = continuous_dividend_increase_years(_continuous_dividend_increase_years, dividend&.indicated_dividend, indicated_dividend, n == 10)
-      dividend = Dividend.create(
+      dividend = Dividend.create!(
         brand_fiscal_year: brand_fiscal_year,
         indicated_dividend: indicated_dividend,
         surplus_dividend: surplus_dividend,
@@ -39,7 +43,9 @@ ActiveRecord::Base.transaction do
     end
   end
 
+  brand_latest_dividends = []
   Brand.all.each do |brand|
-    BrandLatestDividend.create(brand: brand, dividend: brand.latest_dividend, fiscal_year: brand.latest_dividend.fiscal_year_date)
+    brand_latest_dividends << BrandLatestDividend.new(brand: brand, dividend: brand.latest_dividend, fiscal_year: brand.latest_dividend.fiscal_year_date)
   end
+  BrandLatestDividend.import!(brand_latest_dividends)
 end
