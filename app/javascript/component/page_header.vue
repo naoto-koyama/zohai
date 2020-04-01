e9769f78a448955b7aae4388f2d58237f57f2be6
児山直人 <naoto.koyama@uhuru.jp>
2020-04-01 23:14:01 +0900

    layoutにあったCSSを一部projectに移動

--- app/javascript/component/page_header.vue
+++ app/javascript/component/page_header.vue
@@ -1,9 +1,11 @@
 <template>
   <nav class="l-nav grey darken-2">
-    <span class="l-nav__title">Kabu</span>
+    <span class="p-title">Kabu</span>
     <ul>
       <li class="l-nav__search">
         <input type="text" placeholder="Code or Name" v-model="search_text" @input="changeSearch()">
+      <li class="p-search-text">
+        <input type="text" placeholder="コード / 銘柄" v-model="search_text" @input="changeSearch()">
         <i class="material-icons">search</i>
       </li>
     </ul>
@@ -29,6 +31,33 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .p-title {
+    font-size: 50px;
+    font-family: 'Indie Flower', cursive;
+    display: flex;
 
+    &:before {
+     content: '';
+     background: #f57c00;
+     width: 15px;
+     height: 100%;
+     border-radius: 5px;
+     margin: 0 10px 0 0;
+    }
+  }
+  
+  .p-search-text {
+    display: flex;
+    align-items: center;
+    input {
+      height: 1.5em;
+      margin: 0;
+      color: #ffffff;
+      font-family: 'Noto Sans JP', 'Roboto', sans-serif;
+      &::placeholder {
+        opacity: 0.5;
+      }
+    }
+  }
 </style>
